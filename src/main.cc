2ad4e5698f9e824e77ffc734c35c1c8bb9bcd47a
Yantsy <g17870797268@163.com>
2025-10-01 10:11:34 +0800

    学习中

--- src/main.cc
+++ src/main.cc
@@ -1,15 +1,36 @@
 #include <QApplication>
-#include <QGuiApplication>
-#include <QQmlApplicationEngine>
-
+#include <QHBoxLayout>
+#include <QLabel>
+#include <QLineEdit>
+#include <QPushButton>
+#include <QSizePolicy>
+#include <QVBoxLayout>
+#include <QWidget>
+#include <qdrawutil.h>
 int main(int argc, char *argv[]) {
-  QGuiApplication app(argc, argv);
+  QApplication app(argc, argv);
 
-  QQmlApplicationEngine engine;
-  engine.load(QUrl(QStringLiteral("mainwindow.qml"))); // 使用资源路径
+  QWidget window;
+  window.resize(1920, 1080);                   // 设置窗口大小
+  window.setWindowTitle("Swallow And Escape"); // 设置标题
+  window.show();                               // 显示窗口
 
-  if (engine.rootObjects().isEmpty())
-    return -1;
+  QPushButton *button = new QPushButton("Start", &window);
+  button->setFixedSize(window.width() / 4, window.height() / 20);
+  button->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Preferred);
 
-  return app.exec();
+  QLabel *label = new QLabel("Type in your name:");
+
+  QLineEdit *lineedit = new QLineEdit("have fun");
+
+  QVBoxLayout *mainlayout = new QVBoxLayout(&window);
+
+  QHBoxLayout *layout = new QHBoxLayout();
+
+  layout->addWidget(label);
+  layout->addWidget(lineedit);
+  mainlayout->addLayout(layout);
+  mainlayout->addWidget(button, 0, Qt::AlignCenter);
+
+  return app.exec(); // 进入事件循环
 }
